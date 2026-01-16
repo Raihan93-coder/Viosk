e2bda5ca22eed7a28ab8b61673668ca035201cca
Akshai Krishna KP <cs24b1096@iiitdm.ac.in>
2026-01-16 11:38:11 +0530

    Initial Setup for Pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,15 @@
-import KioskButton from "./Components/button";
-import ElectricityImage from "./assets/electricity.jpg";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import HomePage from "./Pages/Home";
+import DepartmentsPage from "./Pages/Departments";
 
 function App() {
   return (
-    <>
-      <KioskButton content="Button 1" image={ElectricityImage} />
-      <KioskButton content="Button 2" />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/departments" element={<DepartmentsPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ Frontend/src/Pages/Departments.jsx
@@ -0,0 +1,10 @@
+function DepartmentPage() {
+  return (
+    <div>
+      <h1>Department Page</h1>
+      <p>Welcome to the Department page!</p>
+    </div>
+  );
+}
+
+export default DepartmentPage;
